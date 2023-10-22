2357216a1871f4c97c6db1a8551f82d06f2772e9
veimarcm <vcelis@sena.edu.co>
2023-10-22 18:00:13 -0500

    Ajuste titulo 1 tema 1
    Se ajusta el titulo 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,7 +5,7 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Alistamiento de materiales y equipos de muestreo de calidad de agua
     
     .row.justify-content-center.align-items-center.mb-5
       .col-lg-10
